aabbea120ec2280145cc72d991aa0475eb0df853
nidhiprajapati0820 <nidhi.khurpia@gmail.com>
2022-01-09 08:06:53 -0700

    Initial commit

---
+++ src/main/java/com/simplilearn/EurekaDiscoveryServerDemo/EurekaDiscoveryServerDemoApplication.java
@@ -0,0 +1,15 @@
+package com.simplilearn.EurekaDiscoveryServerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaDiscoveryServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryServerDemoApplication.class, args);
+	}
+
+}
